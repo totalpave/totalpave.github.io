67d32d396aae2bddfe518be0c7585d56050b98e6
Norman Breau <norman.breau@totalpave.com>
2018-09-26 19:38:46 +0000

    updated google maps to version 3.34 as 3.32 will be removed in november

--- js/totalpave_script.js
+++ js/totalpave_script.js
@@ -9,7 +9,7 @@ jQuery(function($) {
             // load google map
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.32&sensor=false&key=AIzaSyDvlcTS8mbGorKNpnj-SrNk7RbUTlpJcNo' +
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.34&sensor=false&key=AIzaSyDvlcTS8mbGorKNpnj-SrNk7RbUTlpJcNo' +
             '&callback=initialize';
         document.body.appendChild(script);
 
